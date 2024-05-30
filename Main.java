f075b1ee2b91429f10f1d894104f7f90b91b389d
iqrahafeez-2 <146338521+iqrahafeez-2@users.noreply.github.com>
2024-05-30 19:51:13 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.example.demo1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("products.fxml"));
+        primaryStage.setTitle("Product Management");
+        primaryStage.setScene(new Scene(root, 450, 250));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
